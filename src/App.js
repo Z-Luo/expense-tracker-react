c5437e6ca876d0de2bd3e953d3ad8271cfd5c83c
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2023-03-28 23:57:00 +1100

    added provider

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { Balance } from "./components/Balance";
 import { IncomeExpense } from "./components/IncomeExpense";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <div className="container">
-        <Balance></Balance>
-        <IncomeExpense></IncomeExpense>
-        <TransactionList></TransactionList>
-        <AddTransaction></AddTransaction>
+    <GlobalProvider>
+      <div className="App">
+        <Header></Header>
+        <div className="container">
+          <Balance></Balance>
+          <IncomeExpense></IncomeExpense>
+          <TransactionList></TransactionList>
+          <AddTransaction></AddTransaction>
+        </div>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
